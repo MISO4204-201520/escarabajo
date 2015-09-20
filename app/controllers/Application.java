077873024980c3dca8d78f08aa46deef5276d316
Henry Mora <hank8617@gmail.com>
2015-09-19 22:45:49 -0500

    Configuración a la base de datos... OK!

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,53 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import play.db.*;
+import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
+	
+	private Connection conn;
+	private Statement st;
 
     public Result index() {
+    	
+    	try{
+    	
+    		conn = DB.getConnection();
+    		
+    		String sql = "INSERT INTO tabla_pruebas (id_prueba, nombre_prueba, telefono_prueba) "
+    				+ "VALUES (2, 'Henry', 01800321654)";
+    		
+    		st = conn.prepareStatement(sql);
+    		
+    		int rows = st.executeUpdate(sql);
+    		
+    		System.out.println("TEST " + rows);
+    		
+    	}
+    	catch(Exception e){
+    		e.printStackTrace();
+    	}
+    	finally{
+    		try {
+				st.close();
+			} 
+    		catch (SQLException e) {
+				e.printStackTrace();
+			}
+    		
+    		try {
+				conn.close();
+			} 
+    		catch (SQLException e) {
+				e.printStackTrace();
+			}
+    	}
+    	
         return ok(index.render("Your new application is ready."));
     }
 
--- build.sbt
+++ build.sbt
@@ -19,3 +19,5 @@ routesGenerator := InjectedRoutesGenerator
 EclipseKeys.projectFlavor := EclipseProjectFlavor.Java           // Java project. Don't expect Scala IDE
 EclipseKeys.createSrc := EclipseCreateSrc.ValueSet(EclipseCreateSrc.ManagedClasses, EclipseCreateSrc.ManagedResources)  // Use .class files instead of generated .scala files for views and routes 
 EclipseKeys.preTasks := Seq(compile in Compile)                  // Compile the project before generating Eclipse files, so that .class files for views and routes are present
+
+libraryDependencies += "org.postgresql" % "postgresql" % "9.3-1100-jdbc4"
