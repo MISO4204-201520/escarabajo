9d41ba75fb64b5bb1ba4a93d964368ca2c7c2fb0
Henry Mora <hank8617@gmail.com>
2015-09-21 22:29:15 -0500

    Creación de paquetes y arreglo de conexión

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,49 +4,22 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import play.db.*;
+import database.Connector;
 import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
 	
-	private Connection conn;
-	private Statement st;
-
     public Result index() {
+    
+    	Connection c = Connector.getConnection();
     	
-    	try{
-    	
-    		conn = DB.getConnection();
-    		
-    		String sql = "INSERT INTO tabla_pruebas (id_prueba, nombre_prueba, telefono_prueba) "
-    				+ "VALUES (2, 'Henry', 01800321654)";
-    		
-    		st = conn.prepareStatement(sql);
-    		
-    		int rows = st.executeUpdate(sql);
-    		
-    		System.out.println("TEST " + rows);
-    		
-    	}
-    	catch(Exception e){
-    		e.printStackTrace();
-    	}
-    	finally{
-    		try {
-				st.close();
-			} 
-    		catch (SQLException e) {
-				e.printStackTrace();
-			}
-    		
-    		try {
-				conn.close();
-			} 
-    		catch (SQLException e) {
-				e.printStackTrace();
-			}
-    	}
+    	try {
+			Statement st = c.createStatement();
+		} 
+    	catch (SQLException e) {
+			e.printStackTrace();
+		}
     	
         return ok(index.render("Your new application is ready."));
     }
---
+++ app/views/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author HMORAVIL
+ *
+ */
+package views;
