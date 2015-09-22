bfb6fa815d54f5a0279b8808c1bdc208179295bf
deisydiazp <deisydiazp@gmail.com>
2015-09-22 10:03:26 -0500

    Ajuste clase de conexion base de datos , primera consulta recorridos

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,8 +3,11 @@ package controllers;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
+import model.Recorrido;
 import database.Connector;
+import database.RecorridoDAO;
 import play.mvc.*;
 import views.html.*;
 
@@ -13,7 +16,10 @@ public class Application extends Controller {
     public Result index() {
     
     	Connection c = Connector.getConnection();
+    	RecorridoDAO rec = new RecorridoDAO();
+    	List<Recorrido> lst = rec.consultarRecorridos(c);
     	
+    	System.out.println(lst);
     	try {
 			Statement st = c.createStatement();
 		} 
--- app/database/Connector.java
+++ app/database/Connector.java
@@ -1,34 +1,49 @@
 package database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 import play.db.DB;
 
 public class Connector {
-	
-	private static Connection conn = null;
-	
-	public static Connection getConnection(){
 
-    	try{    	
-    		if(conn == null){
-    			conn = DB.getConnection();
-    		}
-    	}
-    	catch(Exception e){
-    		e.printStackTrace();
-    	}
-    	finally{
-    		try {
-				conn.close();
-			} 
-    		catch (SQLException e) {
-				e.printStackTrace();
+	private static Connection conn = null;
+
+	public static Connection getConnection() {
+
+		try {
+			if (conn == null) {
+				conn = DB.getConnection();
 			}
-    	}
-    	
-    	return conn;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
 	}
 
+	public static void cerrarConexion(Connection con) {
+		try {
+			if(con != null)
+				con.close();
+		} catch (SQLException e) {
+		}
+	}
+
+	public static void cerrarResultSet(ResultSet res) {
+		try {
+			if(res != null)
+				res.close();
+		} catch (SQLException e) {
+		}
+	}
+	
+	public static void cerrarStatement(PreparedStatement pre) {
+		try {
+			if(pre != null)
+				pre.close();
+		} catch (SQLException e) {
+		}
+	}
 }
