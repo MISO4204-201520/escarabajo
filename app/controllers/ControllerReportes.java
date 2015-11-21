8601d5918c05330b14eaf11132a2aa666c006d7a
Henry Mora <hank8617@gmail.com>
2015-11-21 14:23:26 -0500

    Se arregla el ordenamiento de las métricas en los reporte y el llamado del reporte de métricas e histórico desde los views

--- app/controllers/ControllerReportes.java
+++ app/controllers/ControllerReportes.java
@@ -49,6 +49,35 @@ public class ControllerReportes extends Controller{
 		}
 	}
 	
+	public static Result generarReporteHistoricoPDF(){
+		
+		String fileName="./app/reports/ReporteHistoricos";
+
+		try {
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			
+			User usuario = Application.getLocalUser(session());
+			
+			parameters.put("p_usuario", usuario.id);
+			parameters.put("p_fecha", new Date());
+	
+			JasperCompileManager.compileReportToFile(fileName + ".jrxml");
+			
+			String printFileName = JasperFillManager.fillReportToFile(fileName + ".jasper" , parameters, DB.getConnection());
+			
+			if (printFileName != null) {
+				JasperExportManager.exportReportToPdfFile(printFileName, fileName + ".pdf");
+			}
+		
+			return ok(new File(fileName + ".pdf"));
+
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 	public static Result generarReporteRecorridoPDF(Long idRecorrido){
 		
 		String fileName="./app/reports/ReporteRecorrido";
